cad72ceeca487134cc9e405d15279dadec87ccf8
MajidFatima <fatima.majid@he-arc.ch>
2019-04-08 19:37:20 +0200

    Add 2 previous examples

---
+++ src/main/java/ch/hearc/odi/koulutus/exception/MarathonException.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2019. Cours outils de développement intégré, HEG-Arc
+ */
+
+package ch.hearc.odi.koulutus.exception;
+
+public class MarathonException extends Exception {
+
+  public MarathonException(String message) {
+    super(message);
+  }
+}
---
+++ src/main/java/ch/hearc/odi/koulutus/exception/PersonException.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2019. Cours outils de développement intégré, HEG-Arc
+ */
+
+package ch.hearc.odi.koulutus.exception;
+
+public class PersonException extends Exception {
+
+  public PersonException(String message) {
+    super(message);
+  }
+}
