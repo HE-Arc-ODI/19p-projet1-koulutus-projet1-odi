875217c7fb1379eaec36cd1b04a6979893841055
MajidFatima <fatima.majid@he-arc.ch>
2019-04-08 20:16:56 +0200

    Update Course.java

--- src/main/java/ch/hearc/odi/koulutus/business/Course.java
+++ src/main/java/ch/hearc/odi/koulutus/business/Course.java
@@ -1,12 +1,24 @@
 package ch.hearc.odi.koulutus.business;
 
+import java.io.Serializable;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.xml.bind.annotation.XmlRootElement;
+import org.hibernate.annotations.GenericGenerator;
 
 @Entity
 @Table(name = "Course")
 @XmlRootElement(name = "Course")
-public class Course {
+public class Course implements Serializable {
+  private Integer id;
 
+
+  @Id
+  @GeneratedValue(generator = "increment")
+  @GenericGenerator(name = "increment", strategy = "increment")
+  public Integer getId() {
+    return id;
+  }
 }
