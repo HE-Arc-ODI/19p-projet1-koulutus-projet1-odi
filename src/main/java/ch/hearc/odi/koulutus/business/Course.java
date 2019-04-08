8d0d8f9439cfa537e2374738c86119743d3ebbd8
MajidFatima <fatima.majid@he-arc.ch>
2019-04-08 20:16:03 +0200

    Update Course.java

--- src/main/java/ch/hearc/odi/koulutus/business/Course.java
+++ src/main/java/ch/hearc/odi/koulutus/business/Course.java
@@ -1,5 +1,12 @@
 package ch.hearc.odi.koulutus.business;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@Entity
+@Table(name = "Course")
+@XmlRootElement(name = "Course")
 public class Course {
 
 }
