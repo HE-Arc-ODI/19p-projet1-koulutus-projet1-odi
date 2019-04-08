4059bec2e775c6ce9e50ef65a54c0dd31b255771
MajidFatima <fatima.majid@he-arc.ch>
2019-04-08 19:41:51 +0200

    Update PersonException.java

--- src/main/java/ch/hearc/odi/koulutus/exception/PersonException.java
+++ src/main/java/ch/hearc/odi/koulutus/exception/PersonException.java
@@ -4,9 +4,9 @@
 
 package ch.hearc.odi.koulutus.exception;
 
-public class Participant extends Exception {
+public class PersonException extends Exception {
 
-  public ParticipantException(String message) {
+  public PersonException(String message) {
     super(message);
   }
 }
