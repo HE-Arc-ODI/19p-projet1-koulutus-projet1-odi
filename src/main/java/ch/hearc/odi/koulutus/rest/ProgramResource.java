a001d8542945a822e974236434f769bfa76c3380
MajidFatima <fatima.majid@he-arc.ch>
2019-04-15 15:13:18 +0200

    Update ProgramResource.java
    - add comments
    - get programs by Id

--- src/main/java/ch/hearc/odi/koulutus/rest/ProgramResource.java
+++ src/main/java/ch/hearc/odi/koulutus/rest/ProgramResource.java
@@ -8,6 +8,7 @@ import javax.inject.Inject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -19,9 +20,23 @@ public class ProgramResource {
   @Inject
   private PersistenceService persistenceService;
 
+  /*
+  Get all programs
+   */
   @GET
   public List<Program> programGet() {
     return persistenceService.getPrograms();
   }
+
+  /*
+  Get program By Id
+   */
+  @GET
+  @Path("{programId}")
+  public Program getProgramById(@PathParam("programId") Integer programId) {
+    return persistenceService.getProgramById(programId);
+  }
+
+
 
 }
