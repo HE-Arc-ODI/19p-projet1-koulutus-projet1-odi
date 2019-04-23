42fd14d6f4f7e7a5cff025151d64cec8895b9979
MajidFatima <fatima.majid@he-arc.ch>
2019-04-23 08:40:25 +0200

    Update ParticipantResource.java

--- src/main/java/ch/hearc/odi/koulutus/rest/ParticipantResource.java
+++ src/main/java/ch/hearc/odi/koulutus/rest/ParticipantResource.java
@@ -2,12 +2,20 @@ package ch.hearc.odi.koulutus.rest;
 
 import ch.hearc.odi.koulutus.business.Participant;
 import ch.hearc.odi.koulutus.services.PersistenceService;
+import java.text.ParseException;
+import java.util.Date;
 import java.util.List;
 import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.NotFoundException;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 
 @Path("participant")
@@ -22,5 +30,17 @@ public class ParticipantResource {
   public List<Participant> personGet() {
     return persistenceService.getParticipant();
   }
+
+/*
+  @GET
+  @Path("{id}")
+  public Participant getParticipant(@Context HttpServletRequest servletRequest, @PathParam("id") Integer id) {
+    try {
+      return persistenceService.getP(id);
+    } catch (PersonException e) {
+      e.printStackTrace();
+      throw new NotFoundException("the person does not exist.");
+    }
+  }*/
 
 }
