823ac9166838b8511fdd3a4e7769175ad96dceb3
Carmine Perna <carmine.pernagutierrez@he-arc.ch>
2019-04-24 23:03:44 +0200

    ParticipantResource - getAllParticipant implemented

--- src/main/java/ch/hearc/odi/koulutus/rest/ParticipantResource.java
+++ src/main/java/ch/hearc/odi/koulutus/rest/ParticipantResource.java
@@ -1,10 +1,21 @@
 package ch.hearc.odi.koulutus.rest;
 
+import ch.hearc.odi.koulutus.business.Participant;
+import ch.hearc.odi.koulutus.services.PersistenceService;
+
+import javax.inject.Inject;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import java.util.ArrayList;
 
 @Path("participant")
 @Produces(MediaType.APPLICATION_JSON)
 public class ParticipantResource {
+    @Inject
+    PersistenceService persistenceService;
 
+    @GET
+    public ArrayList<Participant> getAllParticipant(){
+        return persistenceService.getParticipant();
+    }
 }
