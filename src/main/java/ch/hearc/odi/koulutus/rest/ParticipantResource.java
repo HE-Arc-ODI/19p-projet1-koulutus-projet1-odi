5729c81f00ce192e21ab9302eab8e9d7afb8bb1c
Carmine Perna <carmine.pernagutierrez@he-arc.ch>
2019-04-24 23:00:52 +0200

    ParticipantResource - class created
    ResourceLoader
    - updated to include this file

---
+++ src/main/java/ch/hearc/odi/koulutus/rest/ParticipantResource.java
@@ -0,0 +1,10 @@
+package ch.hearc.odi.koulutus.rest;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+
+@Path("participant")
+@Produces(MediaType.APPLICATION_JSON)
+public class ParticipantResource {
+
+}
