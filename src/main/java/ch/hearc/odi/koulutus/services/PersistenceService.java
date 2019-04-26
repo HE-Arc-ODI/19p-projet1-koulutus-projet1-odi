b0bbe2a58b0c136e738b09ff272027a546d3f8a4
MajidFatima <fatima.majid@he-arc.ch>
2019-04-26 14:18:20 +0200

    Update PersistenceService.java

--- src/main/java/ch/hearc/odi/koulutus/services/PersistenceService.java
+++ src/main/java/ch/hearc/odi/koulutus/services/PersistenceService.java
@@ -40,8 +40,6 @@ public class PersistenceService {
     entityManager.getTransaction().begin();
     List<Program> programs = entityManager.createQuery("from Program", Program.class)
         .getResultList();
-    /*entityManager.getTransaction().commit();
-    entityManager.close();*/
     return (ArrayList<Program>) programs;
   }
 
